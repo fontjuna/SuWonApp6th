d3b03e8df7802ab2211ce4ff0c37462580a52b8c
fontjuna <fontjuna@hanmail.net>
2017-07-18 14:20:22 +0900

    커피주문 레이아웃

---
+++ app/src/main/java/hwa/seung/noh/suwonapp6th/CoffeeActivity.java
@@ -0,0 +1,13 @@
+package hwa.seung.noh.suwonapp6th;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CoffeeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coffee);
+    }
+}
