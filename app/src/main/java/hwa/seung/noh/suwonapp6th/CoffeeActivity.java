d3c194e870ce578e97569912cfe39cd78b04f300
fontj <fontjuna@hanmail.net>
2017-07-22 17:42:21 +0900

    커피주문자명 및 토핑추가, 주문내역 인쇄

--- app/src/main/java/hwa/seung/noh/suwonapp6th/CoffeeActivity.java
+++ app/src/main/java/hwa/seung/noh/suwonapp6th/CoffeeActivity.java
@@ -3,6 +3,8 @@ package hwa.seung.noh.suwonapp6th;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
@@ -11,12 +13,15 @@ public class CoffeeActivity extends AppCompatActivity {
 
     private final int UNIT_PRICE = 3000;
     private final int MINIMUM_QTY = 1;
+
+    private EditText mNameEditText;
+    private CheckBox mWhippingCreamChecked;
     private TextView mQuantityTextView;
     private TextView mPriceTextView;
+
     private int mQuantity = MINIMUM_QTY;
-    private DecimalFormat df = new DecimalFormat("#,###");
-//    private Button mPlusButton;
-//    private Button mMinusButton;
+
+    private DecimalFormat df = new DecimalFormat("#,##0");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,29 +30,38 @@ public class CoffeeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_coffee);
 
         // XML에 있는 View의 레퍼런스를 가져오는 방법
-//        mMinusButton = (Button) findViewById(R.id.minus_button);
-//        mPlusButton = (Button) findViewById(R.id.plus_button);
+        mNameEditText = (EditText) findViewById(R.id.name_edit);
+        mWhippingCreamChecked = (CheckBox) findViewById(R.id.whipping_cream_check);
         mQuantityTextView = (TextView) findViewById(R.id.quantity_text);
         mPriceTextView = (TextView) findViewById(R.id.price_text);
-        // mQuantityTextView.setText(String.valueOf(mQuantity));
-        mQuantityTextView.setText(mQuantity + "");
+
         priceView();
     }
 
     public void minusButtonClicked(View view) {
         //Toast.makeText(this,"마이너스",Toast.LENGTH_SHORT).show();
         if (mQuantity > MINIMUM_QTY) {
-            mQuantityTextView.setText(--mQuantity + "");
+            mQuantity--;
         }
         priceView();
     }
 
     public void plusButtonClicked(View view) {
-        mQuantityTextView.setText(++mQuantity + "");
+        mQuantity++;
         priceView();
     }
 
     private void priceView() {
-        mPriceTextView.setText(df.format(mQuantity * UNIT_PRICE) + "");
+        mQuantityTextView.setText(mQuantity + "");
+        String message = "주문자 : " + mNameEditText.getText().toString();
+        message += "\n========================";
+        message += "\n휘핑크림 추가 : " + mWhippingCreamChecked.isChecked();
+        message += "\n갯수 : " + mQuantity;
+        message += "\n가격 : " + df.format(mQuantity * UNIT_PRICE + (mWhippingCreamChecked.isChecked() ? 1000 : 0)) + "원";
+        mPriceTextView.setText(message);
+    }
+
+    public void onCheckBoxClicked(View view) {
+        priceView();
     }
 }
