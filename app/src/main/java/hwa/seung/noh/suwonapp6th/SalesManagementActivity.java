b8902d05fb26124e9107a0982f0512ef95ccdcf2
fontjuna <fontjuna@gmail.com>
2017-07-24 13:33:02 +0900

    왔다 갔다 정보 주고받기 다른 또하나 영업관리에서 구현

--- app/src/main/java/hwa/seung/noh/suwonapp6th/SalesManagementActivity.java
+++ app/src/main/java/hwa/seung/noh/suwonapp6th/SalesManagementActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+/*
 public class SalesManagementActivity extends AppCompatActivity {
 
     public static final int REQUEST_CODE_LOGIN = 1000;
@@ -36,6 +37,66 @@ public class SalesManagementActivity extends AppCompatActivity {
             Toast.makeText(this, data.getStringExtra("result").toString(), Toast.LENGTH_SHORT).show();
         }
     }
+}
+*/
+public class SalesManagementActivity extends AppCompatActivity implements View.OnClickListener {
 
+    public static final int REQUEST_CODE_LOGIN = 1000;
+    public static final int REQUEST_CODE_SIGN_UP = 2000;
+
+    private EditText etId;
+    private EditText etPassWord;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sales_management);
+
+        etId = (EditText) findViewById(R.id.et_id);
+        etPassWord = (EditText) findViewById(R.id.et_password);
+
+        findViewById(R.id.btn_login).setOnClickListener(this);
+        findViewById(R.id.sign_up_text).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_login:
+                login();
+                break;
+            case R.id.sign_up_text:
+                signup();
+                break;
+        }
+        EditText etId = (EditText) findViewById(R.id.et_id);
+        EditText etPassWord = (EditText) findViewById(R.id.et_password);
+    }
+
+    private void signup() {
+        Intent intent = new Intent(SalesManagementActivity.this, SalesManagementOtherActivity.class);
+        startActivityForResult(intent, REQUEST_CODE_SIGN_UP);
+    }
+
+
+    private void login() {
+        Intent intent = new Intent(SalesManagementActivity.this, SalesManagementOtherActivity.class);
+        intent.putExtra("id", etId.getText().toString());
+        intent.putExtra("password", etPassWord.getText().toString());
+        startActivityForResult(intent, REQUEST_CODE_LOGIN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_CODE_LOGIN &&
+                    data != null) {
+                Toast.makeText(this, data.getStringExtra("result").toString(), Toast.LENGTH_SHORT).show();
+            } else if (requestCode == REQUEST_CODE_SIGN_UP) {
+                Toast.makeText(this, "가입 잘 되었음", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
 }
--- app/src/main/java/hwa/seung/noh/suwonapp6th/SalesManagementOtherActivity.java
+++ app/src/main/java/hwa/seung/noh/suwonapp6th/SalesManagementOtherActivity.java
@@ -4,39 +4,40 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class SalesManagementOtherActivity extends AppCompatActivity {
+public class SalesManagementOtherActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sales_management_other);
 
-        String message = "ID : ";
-        Intent intent = getIntent();
-        message += intent.getStringExtra("id") + "\nPW : ";
-        message += intent.getStringExtra("password");
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        findViewById(R.id.btn_customer_management).setOnClickListener(this);
+        findViewById(R.id.btn_revenue_management).setOnClickListener(this);
+        findViewById(R.id.btn_product_management).setOnClickListener(this);
 
+        findViewById(R.id.result2_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(RESULT_OK);
+                finish();
+            }
+        });
+
+        if (getIntent() != null) {
+            String id = getIntent().getStringExtra("id");
+            String password = getIntent().getStringExtra("password");
+            Toast.makeText(this, "ID  : " + id + "\nPW : " + password, Toast.LENGTH_SHORT).show();
+        }
     }
 
-    public void onClick_menu_button(View view) {
+    @Override
+    public void onClick(View v) {
         Intent intent = new Intent();
-        switch (view.getId()) {
-            case R.id.btn_customer_management: {
-                intent.putExtra("result", "고객관리");
-                break;
-            }
-            case R.id.btn_revenue_management: {
-                intent.putExtra("result", "매출관리");
-                break;
-            }
-            case R.id.btn_product_management: {
-                intent.putExtra("result", "상품관리");
-                break;
-            }
-        }
+        String text = ((Button) v).getText().toString();
+        intent.putExtra("text", text);
         setResult(RESULT_OK, intent);
         finish();
     }
