9403232c2da0506c62dfd3314e2b430d73926878
fontjuna <fontjuna@gmail.com>
2017-07-29 10:36:52 +0900

    안드로이드 9일차 개시 전

--- app/src/main/java/hwa/seung/noh/suwonapp6th/SalesManagementActivity.java
+++ app/src/main/java/hwa/seung/noh/suwonapp6th/SalesManagementActivity.java
@@ -8,6 +8,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 /*
+
+// 세번째방법
 public class SalesManagementActivity extends AppCompatActivity {
 
     public static final int REQUEST_CODE_LOGIN = 1000;
@@ -20,25 +22,33 @@ public class SalesManagementActivity extends AppCompatActivity {
         findViewById(R.id.btn_login).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText etId = (EditText) findViewById(R.id.et_id);
-                EditText etPassWord = (EditText) findViewById(R.id.et_password);
-                Intent intent = new Intent(SalesManagementActivity.this, SalesManagementOtherActivity.class);
-                intent.putExtra("id", etId.getText().toString());
-                intent.putExtra("password", etPassWord.getText().toString());
-                startActivityForResult(intent, REQUEST_CODE_LOGIN);
             }
         });
     }
 
+}
+
+// 네번째 방법 - 세번째 방법을 아래와 같이 변경
+// 무명을래스를 인터페이스를 구현하는 방법으로 변경
+public class SalesManagementActivity extends AppCompatActivity implements View.OnClickListener {
+
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE_LOGIN && resultCode == RESULT_OK && data != null) {
-            Toast.makeText(this, data.getStringExtra("result").toString(), Toast.LENGTH_SHORT).show();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sales_management);
+
+        findViewById(R.id.btn_login).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_login :
         }
     }
 }
 */
+
 public class SalesManagementActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final int REQUEST_CODE_LOGIN = 1000;
@@ -54,8 +64,8 @@ public class SalesManagementActivity extends AppCompatActivity implements View.O
 
         etId = (EditText) findViewById(R.id.et_id);
         etPassWord = (EditText) findViewById(R.id.et_password);
-
         findViewById(R.id.btn_login).setOnClickListener(this);
+
         findViewById(R.id.sign_up_text).setOnClickListener(this);
     }
 
