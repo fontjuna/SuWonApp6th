f4c10abfa0fb95720786d7443e53cd91aca84971
fontjuna <fontjuna@gmail.com>
2017-07-29 18:40:25 +0900

    리스트뷰 아이템 클릭 이벤트 : OnItemClickListener 구현방법

--- app/src/main/java/hwa/seung/noh/suwonapp6th/adapterview/AdapterViewExamActivity.java
+++ app/src/main/java/hwa/seung/noh/suwonapp6th/adapterview/AdapterViewExamActivity.java
@@ -2,9 +2,12 @@ package hwa.seung.noh.suwonapp6th.adapterview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -20,14 +23,14 @@ public class AdapterViewExamActivity extends AppCompatActivity {
         // view
         ListView listView = (ListView) findViewById(R.id.list_view);
         GridView gridView = (GridView) findViewById(R.id.grid_view);
-        Spinner spinner =(Spinner) findViewById(R.id.spinner);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
 
         // data
-        ArrayList<People> data = new ArrayList<>();
+        final ArrayList<People> data = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
             int picture;
             if (i % 2 == 0) {
-                picture = R.drawable.bonfire_night;
+                picture = R.drawable.ic_launcher;
             } else {
                 picture = R.drawable.ic_touch_app_black_24dp;
             }
@@ -41,5 +44,17 @@ public class AdapterViewExamActivity extends AppCompatActivity {
         listView.setAdapter(adapter);
         gridView.setAdapter(adapter);
         spinner.setAdapter(adapter);
+
+        //OnItemClickListnser
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                People people = data.get(position);
+                People people = (People) parent.getAdapter().getItem(position);
+                Toast.makeText(AdapterViewExamActivity.this, people.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
