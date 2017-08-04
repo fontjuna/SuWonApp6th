fc297aefea03c7b0bba3bf42c13f6f41e6b6afd9
fontjuna <fontjuna@gmail.com>
2017-08-04 23:32:48 +0900

    커피앱 ORDER버튼으로 이메일 보내기

--- app/src/main/java/hwa/seung/noh/suwonapp6th/CoffeeActivity.java
+++ app/src/main/java/hwa/seung/noh/suwonapp6th/CoffeeActivity.java
@@ -1,5 +1,7 @@
 package hwa.seung.noh.suwonapp6th;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -63,5 +65,16 @@ public class CoffeeActivity extends AppCompatActivity {
 
     public void onCheckBoxClicked(View view) {
         priceView();
+    }
+
+    public void onOrderButtonClicked(View view) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[] {"fontjuna@naver.com"});
+        intent.putExtra(Intent.EXTRA_SUBJECT, "커피주문");
+        intent.putExtra(Intent.EXTRA_TEXT, mPriceTextView.getText());
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
