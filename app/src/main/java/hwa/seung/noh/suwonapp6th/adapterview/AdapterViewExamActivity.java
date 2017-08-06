999e6a26a205173c3abf6fbf1e4bdf062d8dc648
fontjuna <fontjuna@gmail.com>
2017-08-07 05:46:50 +0900

    리스트뷰 : 옵션메뉴, 컨텍스트메뉴달고 선택시 삭제기능 구현

--- app/src/main/java/hwa/seung/noh/suwonapp6th/adapterview/AdapterViewExamActivity.java
+++ app/src/main/java/hwa/seung/noh/suwonapp6th/adapterview/AdapterViewExamActivity.java
@@ -3,6 +3,10 @@ package hwa.seung.noh.suwonapp6th.adapterview;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
@@ -16,6 +20,9 @@ import hwa.seung.noh.suwonapp6th.R;
 
 public class AdapterViewExamActivity extends AppCompatActivity {
 
+    ArrayList<People> mPeopleData;
+    PeopleAdapter mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +33,7 @@ public class AdapterViewExamActivity extends AppCompatActivity {
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
 
         // 1단계 : 데이타 준비
-        ArrayList<People> data = new ArrayList<>();
+        mPeopleData = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
             int picture;
             if (i % 2 == 0) {
@@ -35,16 +42,16 @@ public class AdapterViewExamActivity extends AppCompatActivity {
                 picture = R.drawable.ic_touch_app_black_24dp;
             }
             People people = new People("아무개 " + i, "전화번호 " + i, picture);
-            data.add(people);
+            mPeopleData.add(people);
         }
 
         // 2단계 : 어댑터 준비
-        PeopleAdapter adapter = new PeopleAdapter(AdapterViewExamActivity.this, data);
+        mAdapter = new PeopleAdapter(AdapterViewExamActivity.this, mPeopleData);
 
         // 3단계 : 연결
-        listView.setAdapter(adapter);
-        gridView.setAdapter(adapter);
-        spinner.setAdapter(adapter);
+        listView.setAdapter(mAdapter);
+        gridView.setAdapter(mAdapter);
+        spinner.setAdapter(mAdapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -59,14 +66,77 @@ public class AdapterViewExamActivity extends AppCompatActivity {
             }
         });
 
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(AdapterViewExamActivity.this, "롱 클릭", Toast.LENGTH_SHORT).show();
-                return true;
-            }
-        });
+//        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+//            @Override
+//            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+//                Toast.makeText(AdapterViewExamActivity.this, "롱 클릭", Toast.LENGTH_SHORT).show();
+//                return true;
+//            }
+//        });
+
+        registerForContextMenu(listView);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        // 메뉴버튼이 처음 눌러졌을 때 실행되는 콜백메서드
+        // 메뉴버튼을 눌렀을 때 보여줄 menu 에 대해서 정의
+        getMenuInflater().inflate(R.menu.menu_address, menu);
+        Log.d("test", "onCreateOptionsMenu - 최초 메뉴키를 눌렀을 때 호출됨");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // 메뉴의 항목을 선택(클릭)했을 때 호출되는 콜백메서드
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        Log.d("test", "onOptionsItemSelected - 메뉴항목을 클릭했을 때 호출됨");
+
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.menu_add:
+                Intent intent = new Intent(AdapterViewExamActivity.this, DetailAddressActivity.class);
+                startActivity(intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        // 컨텍스트 메뉴가 최초로 한번만 호출되는 콜백 메서드
+        Log.d("test", "onCreateContextMenu");
+        getMenuInflater().inflate(R.menu.coffee_menu, menu);
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)
+                item.getMenuInfo();
+        // 롱클릭했을 때 나오는 context Menu 의 항목을 선택(클릭) 했을 때 호출
+        switch (item.getItemId()) {
+            case R.id.item1_menu:
+                Toast.makeText(this, "1 번 클릭", Toast.LENGTH_SHORT).show();
+                //삭제
+                mPeopleData.remove(info.position);
+                //데이타가 변경되었음을 에댑터에게 통지하여 화면 갱신
+                mAdapter.notifyDataSetChanged();
+
+                return true;
+            case R.id.item2_menu:
+                Toast.makeText(this, "2 번 클릭", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+
+    }
 
 }
+
