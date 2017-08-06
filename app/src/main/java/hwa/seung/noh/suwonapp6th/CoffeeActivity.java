c6adee13c01a405e1f621b1802b71c69c010cff4
fontjuna <fontjuna@gmail.com>
2017-08-06 20:40:43 +0900

    앱바에 옵션메뉴 달기 완성 (커피앱 이용)

--- app/src/main/java/hwa/seung/noh/suwonapp6th/CoffeeActivity.java
+++ app/src/main/java/hwa/seung/noh/suwonapp6th/CoffeeActivity.java
@@ -4,10 +4,14 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.DecimalFormat;
 
@@ -72,11 +76,38 @@ public class CoffeeActivity extends AppCompatActivity {
     public void onOrderButtonClicked(View view) {
         Intent intent = new Intent(Intent.ACTION_SENDTO);
         intent.setData(Uri.parse("mailto:")); // only email apps should handle this
-        intent.putExtra(Intent.EXTRA_EMAIL, new String[] {"fontjuna@naver.com"});
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"fontjuna@naver.com"});
         intent.putExtra(Intent.EXTRA_SUBJECT, "커피주문");
         intent.putExtra(Intent.EXTRA_TEXT, mPriceTextView.getText());
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.coffee_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1_menu:
+                startActivity(new Intent(this,BasketBallActivity.class));
+                return true;
+            case R.id.item2_menu:
+                startActivity(new Intent(this,SalesManagementActivity.class));
+                return true;
+            case R.id.item3_menu:
+                Toast.makeText(this, "메뉴3번을 클릭 했습니다.", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item4_menu:
+                Toast.makeText(this, "잘했어요!", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
