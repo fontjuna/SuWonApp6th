f52fe52670c4e85970aa0ea277b184c1bd6a0cfc
fontjuna <fontjuna@gmail.com>
2017-08-07 03:36:30 +0900

    리스트뷰의 아이템 클릭시 반응하는 화면 생성

---
+++ app/src/main/java/hwa/seung/noh/suwonapp6th/adapterview/DetailAddressActivity.java
@@ -0,0 +1,19 @@
+package hwa.seung.noh.suwonapp6th.adapterview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import hwa.seung.noh.suwonapp6th.R;
+
+/**
+ * Created by fontjuna on 2017-08-07.
+ */
+
+public class DetailAddressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_address);
+    }
+}
