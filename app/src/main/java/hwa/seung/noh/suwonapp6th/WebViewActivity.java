0075aa1a79df3a17bc6416e8b845de8533cebcd1
fontjuna <fontjuna@gmail.com>
2017-08-06 13:32:16 +0900

    웹뷰 - 코딩 (실습완료)

--- app/src/main/java/hwa/seung/noh/suwonapp6th/WebViewActivity.java
+++ app/src/main/java/hwa/seung/noh/suwonapp6th/WebViewActivity.java
@@ -2,12 +2,46 @@ package hwa.seung.noh.suwonapp6th;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
 
 public class WebViewActivity extends AppCompatActivity {
+
+    WebView mWebView;
+    EditText mUrlEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
+
+        mWebView = (WebView) findViewById(R.id.window_web_view);
+        mUrlEditText = (EditText) findViewById(R.id.url_edit_text);
+        mWebView.setWebViewClient(new WebViewClient());
+
+    }
+
+    public void onMoveButtonClicked(View view) {
+        String url = mUrlEditText.getText().toString();
+        mWebView.loadUrl(url);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mWebView.canGoBack()){
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    public void onBackButtonClicked(View view) {
+        mWebView.goBack();
+    }
+
+    public void onForwardButtonClicked(View view) {
+        mWebView.goForward();
     }
 }
