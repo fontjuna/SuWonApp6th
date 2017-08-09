f1c784a0a80b6399da31bd235bbe18b6f8e4b2b3
fontjuna <fontjuna@gmail.com>
2017-08-10 04:32:59 +0900

    앱리스트정리 - 추가

--- app/src/main/java/hwa/seung/noh/suwonapp6th/MainActivity.java
+++ app/src/main/java/hwa/seung/noh/suwonapp6th/MainActivity.java
@@ -3,6 +3,8 @@ package hwa.seung.noh.suwonapp6th;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -16,8 +18,8 @@ import java.util.List;
 import java.util.Map;
 
 import hwa.seung.noh.suwonapp6th.adapterview.AdapterViewExamActivity;
+import hwa.seung.noh.suwonapp6th.another.AcActivity;
 import hwa.seung.noh.suwonapp6th.another.BballActivity;
-import hwa.seung.noh.suwonapp6th.fragment.Color.ColorActivity;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TITLE = "title";
@@ -38,15 +40,18 @@ public class MainActivity extends AppCompatActivity {
         addItem("커피 앱", "Activity 의 기본, 암시적 인텐트, 옵션 메뉴", CoffeeActivity.class);
         addItem("농구 점수 계산", "상태 저장", BasketBallActivity.class);
         addItem("화면 이동 예제", "데이터 주거니 받거니", MoveActivity.class);
-//        addItem("로그인", "데이터 주거니 받거니 복습", LoginActivity.class);
+        addItem("로그인", "데이터 주거니 받거니 복습", SalesManagementActivity.class);
         addItem("암시적 인텐트", "웹 브라우저, 전화 걸기, 텍스트 전송", ImplicitIntentActivity.class);
         addItem("WebView", "WebView, 뒤로 가기 재정의, ", WebViewActivity.class);
         addItem("회원가입", "RadioGroup", SignUpActivity.class);
-        addItem("AdapterView", "ArrayAdapter, 모델클래스 작성, BaseAdapter, ViewHolder패턴, 아이템 클릭 리스너, Log, 롱클릭 리스너, 옵션 메뉴, Context 메뉴, 어댑터 변경 통지, SharedPreference, AlertDialog, CustomDialog, 리팩토링", AdapterViewExamActivity.class);
+        addItem("AdapterView", "ArrayAdapter, 모델클래스 작성, BaseAdapter, ViewHolder패턴, " +
+                "아이템 클릭 리스너, Log, 롱클릭 리스너, 옵션 메뉴, Context 메뉴, " +
+                "어댑터 변경 통지, SharedPreference, AlertDialog, CustomDialog, " +
+                "리팩토링", AdapterViewExamActivity.class);
         addItem("생명 주기", "Activity Life cycle", LifeCycleActivity.class);
         addItem("상태 저장", "onSaveInstanceState, onRestoreInstanceState", LifeCycleActivity.class);
         addItem("농구 앱 프래그먼트 버전", "Fragment, Callback", BballActivity.class);
-        addItem("프래그먼트 동적 생성", "코드로 Fragment 생성", ColorActivity.class);
+        addItem("프래그먼트 동적 생성", "코드로 Fragment 생성", AcActivity.class);
 //        addItem("과제 : 컬러 프래그먼트", "프래그먼트 추가, 제거", ColorExamActivity.class);
 //        addItem("과제 : 콜백", "액티비티와 프래그먼트 통신", CallbackExamActivity.class);
 //        addItem("좌우로 슬라이딩", "ViewPager", SlidingActivity.class);
@@ -94,19 +99,20 @@ public class MainActivity extends AppCompatActivity {
         mDataList.add(data);
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return super.onCreateOptionsMenu(menu);
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        if (item.getItemId() == R.id.action_sort) {
-//            Collections.reverse(mDataList);
-//            mAdapter.notifyDataSetChanged();
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_sort) {
+            Collections.reverse(mDataList);
+            mAdapter.notifyDataSetChanged();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
